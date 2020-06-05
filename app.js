9dab265ccb72c092296b67c599fbc574c0d2dcf8
ismail <fetayahismail@gmail.com>
2020-06-05 22:14:30 +0200

    jashing password

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const router = require('./router');
+const session = require('express-session');
+const router = require('./router')
 const app = express()
 app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 app.use(express.static('public'))
 app.set('views','view')
 app.set('view engine','ejs')
